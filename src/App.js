395952dfcbd9f49892d9625fbc7eaf83b2887e38
Saif <saifuzzamankhan53@gmail.com>
2022-08-03 17:48:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  //Created date object to initialize the date first..
   let date = new Date().toLocaleTimeString();
 
+  //Created the useState hook to take date variable as a base value and then by using setCurrTime to update it..
   const [currTime, setCurrTime] = useState(date);
 
+  //Created a function to update the time by using setCurrTime...
   const UpdateTime = () => {
     date = new Date().toLocaleTimeString();
     setCurrTime(date)
   }
 
+  //Updating the time by every 1s inside the setInterval..
   setInterval(UpdateTime, 1000)
 
   return (
     <>
-<div className="box">
-
       <h1>{currTime}</h1>
-</div>
-
     </>
   );
 }
